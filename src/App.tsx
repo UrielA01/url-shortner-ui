cb1a3153dcfffe9cdfda58748eeb3e6d4f1bf425
UrielA01 <uriel.azar01@gmail.com>
2023-09-20 23:33:12 +0300

    implement router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 import InputBox from './components/InputBox';
+import ShortUrlRedirect from './components/ShortUrlRedirect';
 
 function App() {
   return (
-    <div className="App">
-      <InputBox />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<InputBox />} />
+          <Route path="/:shortId" element={<ShortUrlRedirect />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
