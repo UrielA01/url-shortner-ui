0a82b2528a2d3660160fcdf82a933b233ebaaa73
UrielA01 <uriel.azar01@gmail.com>
2023-09-20 22:47:01 +0300

    change fetchurl to match ShortUrlRedirect

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -6,7 +6,11 @@ const api = axios.create({
   baseURL,
 });
 
-export const fetchURL = async (urlID: string) => {
+export const fetchURL = async (urlID: string | undefined) => {
+  if (typeof urlID !== 'string' || !urlID) {
+    throw new Error('Invalid URL ID');
+  }
+
   try {
     const response = await api.get(`/url/${urlID}`);
     console.log(response.data);
